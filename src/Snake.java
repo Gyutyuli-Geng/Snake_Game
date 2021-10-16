848156cc37fcd1c3d57d9cca203dd6c9f1410c3e
Saltybusdriver <91703430+Saltybusdriver@users.noreply.github.com>
2021-10-16 02:34:39 +0200

    Point object go br

--- src/Snake.java
+++ src/Snake.java
@@ -3,24 +3,61 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.awt.Point;
 import java.util.*;
 /**
  *
  * @author Kpaco
  */
 public class Snake {
-    int snakeSize;
-    char dir;
-    //probably remade into an array.
-    ArrayList<Integer>Snek=new ArrayList<Integer>();
-    public Snake()
+    int snakeSize=3;
+    char dir='R';
+    Point head=new Point();
+    Point lastbody=new Point();
+    Point nullPoint=new Point();
+    
+    ArrayList<Point>Snek=new ArrayList<Point>();
+    public Snake(int UNIT_SIZE, int QuarterWindow)
     {
-        Snek.add(0,2);
-        for (int i = 1; i < 3; i++)
-        {
-            Snek.add(i,1);
+        nullPoint.setLocation(0,0);
+        head.setLocation(0*UNIT_SIZE,0*UNIT_SIZE);
+        lastbody.setLocation(1*UNIT_SIZE,1*UNIT_SIZE);
+        
+        Snek.add(0,lastbody);
+        for (int i = 0; i <= snakeSize-2; i++) {
+        Snek.add(nullPoint);
         }
+        Snek.add(head);
+        
     }
+    public int getBodyHeadDistance()
+    {
+        return Snek.size()-1;
+    }
+    public void setHeadLocation(int a, int b)
+    {
+        head.setLocation(a,b);
+        Snek.set(Snek.size()-1, head);
+    }
+    public void setTailLocation(int a, int b)
+    {
+        lastbody.setLocation(a,b);
+        Snek.set(0, lastbody);
+    }
+    public double getSnakeHeadX()
+    {
+        return head.getX();
+    }
+    public double getSnakeTailX()
+    {
+        return lastbody.getX();
+    }
+    public void growSnake()
+    {
+        Snek.set(Snek.lastIndexOf(head),nullPoint);
+        Snek.add(head);
+    }
+    
     public void setSnakeSize(int a)
     {
         snakeSize=a;
@@ -37,4 +74,5 @@ public class Snake {
     {
         return Snek.size();
     }
+   
 }
