e7da09ba940094093fc540e4b8e2aacd95679e76
Saltybusdriver <91703430+Saltybusdriver@users.noreply.github.com>
2021-10-27 21:43:24 +0200

    Collision detection

--- src/Level.java
+++ src/Level.java
@@ -14,27 +14,42 @@ import java.util.Random;
 public class Level extends JPanel {
     int Screen_Width;
     int Screen_Height;
-    int UNIT_SIZE=25;
+    int UNIT_SIZE=35;
     int GAME_UNITS;
     int X[];
     int Y[];
-    int SnakeSize=3;
+    int SnakeZize=3;
     int appleX;
     int appleY;
     char direction ='R';
     int obstacleX;
     int obstacleY;
     int xPos;
-    int yPos=0;
-    Snake Player=new Snake(UNIT_SIZE,Calc_QuarterScreen());
+    int yPos;
+    char dir='R';
+    boolean changeDir=false;
+    boolean gameOver=false;
+    Point apple=new Point();
+    Snake Player=new Snake(UNIT_SIZE);
+    Walls Wall=new Walls();
     public Level(int x, int y)
     {
         Screen_Width=x;
         Screen_Height=y;
-        GAME_UNITS=(Screen_Width*Screen_Height/UNIT_SIZE);
-        X=new int[GAME_UNITS];
-        Y=new int[GAME_UNITS];
-        xPos=Calc_QuarterScreen()+Player.getBodyHeadDistance()-1;
+        xPos=Calc_QuarterScreen()+Player.snakeSize-1;
+        Wall.createWall(UNIT_SIZE, Calc_QuarterScreen(), Calc_RightQuarter(), Screen_Height);
+    }
+    public Point GetWallPoint(int i)
+    {
+        return Wall.getArrayPoint(i);
+    }
+    public int getWallDistnaceY(int i)
+    {
+        return (int)Wall.getPointDistnaceY(i);
+    }
+    public int getWallDistnaceX(int i)
+    {
+        return (int)Wall.getPointDistnaceX(i);
     }
     public int Get_Gameunits()
     {
@@ -75,24 +90,100 @@ public class Level extends JPanel {
     
     public void Move()
     {
-         
-        for (int i = Player.getSnakeSize(); i > 0; i--) {
-            
-        }
-       
-        switch(Player.GetSnakeDir())
+        
+        switch(GetSnakeDir())
         {
-            /*/case 'U':
-                X[0]=X[0]+UNIT_SIZE;
-                break;/*/
             case 'R':
                 xPos++;
-                Player.setHeadLocation(xPos*UNIT_SIZE,(int) Player.getSnakeHeadX());
-                Player.setTailLocation((xPos-Player.getBodyHeadDistance())*UNIT_SIZE,(int) Player.getSnakeTailX());
-                
-                
+                if(!Player.checkSnakeGrow()) Player.advanceSnakeBody();
+                else Player.addBody();
+                Player.setHeadLocation(xPos*UNIT_SIZE,(int) Player.getSnakeHeadY());
+                break;
+            case 'D':
+                yPos++;
+                if(!Player.checkSnakeGrow()) Player.advanceSnakeBody();
+                else Player.addBody();
+                Player.setHeadLocation((int) Player.getSnakeHeadX(),yPos*UNIT_SIZE);
+                break;
+            case 'U':
+                yPos--;
+                if(!Player.checkSnakeGrow()) Player.advanceSnakeBody();
+                else Player.addBody();
+                Player.setHeadLocation((int) Player.getSnakeHeadX(),yPos*UNIT_SIZE);
+                break;
+            case 'L':
+                xPos--;
+                if(!Player.checkSnakeGrow())  Player.advanceSnakeBody();
+                else Player.addBody();
+                Player.setHeadLocation(xPos*UNIT_SIZE,(int) Player.getSnakeHeadY());
                 break;
         }
+    }
+    public boolean checkCollision(int i)
+    {
+        if(Player.getHead().equals(Player.getArrayPoint(i)))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+    public boolean checkWallCollision()
+    {
+        int counter=0;
+        for (int i = 0; i < 8; i=i+2) 
+        {
+            if(Player.getHead().getX()==Wall.getArrayPoint(i).getX() )
+            {
+                counter=(int)Wall.getArrayPoint(i).getY();
+                for (int j = 0; j < Wall.getPointDistnaceY(i)/UNIT_SIZE; j++) 
+                {
+                    counter=counter+UNIT_SIZE;
+                    if(Player.getHead().getY()==counter) return true;
+                }
+            }
+            if(Player.getHead().getY()==Wall.getArrayPoint(i).getY())
+            {
+                counter=(int)Wall.getArrayPoint(i).getX();
+                for (int j = 0; j < Wall.getPointDistnaceX(i)/UNIT_SIZE; j++) 
+                {
+                    counter=counter+UNIT_SIZE;
+                    if(Player.getHead().getX()==counter) return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+    public boolean checkAppleGenCoords()
+    {
+        int counter;
+        for (int i = 0; i < 8; i=i+2) 
+        {
+            if(apple.getX()==Wall.getArrayPoint(i).getX())
+            {
+                counter=(int)Wall.getArrayPoint(i).getY();
+                for (int j = 0; j < Wall.getPointDistnaceY(i)/UNIT_SIZE; j++)
+                {
+                    counter=counter+UNIT_SIZE;
+                    if (apple.getY()==counter) return true;
+                }
+            }
+            else if(apple.getY()==Wall.getArrayPoint(i).getY())
+            {
+                counter=(int)Wall.getArrayPoint(i).getX();
+                for (int j = 0; j < Wall.getPointDistnaceX(i)/UNIT_SIZE; j++)
+                {
+                    counter=counter+UNIT_SIZE;
+                    if (apple.getX()==counter) return true;
+                }
+            }
+        }
+        for (int i = 0; i < Player.Snek.size()-1; i++) 
+        {
+            if(apple.equals(Player.Snek.get(i))) return true;
+        }
+        return false;
     }
     public double getSnakeHeadX()
     {
@@ -100,29 +191,55 @@ public class Level extends JPanel {
         help=Player.Snek.get(Player.Snek.size()-1);
         return help.getX();
     }
-    public double getSnakeTailX()
+    public double getBodyX(int index)
+    {
+        return Player.getIBodyPartPointX(index);
+    }
+    public double getBodyY(int index)
+    {
+        return Player.getIBodyPartPointY(index);
+    }
+    public double getSnakeHeadY()
     {
         Point help=new Point();
-        help=Player.Snek.get(0);
-        return help.getX();
+        help=Player.Snek.get(Player.Snek.size()-1);
+        return help.getY();
     }
-    public int getDistancebody()
+    public int getSnakeSize()
     {
-        return Player.getBodyHeadDistance();
+        return Player.getSnakeSize();
     }
-    public int Get_appleX()
+    public void SetSnakeDir(char d)
     {
-        return appleX;
+        dir=d;
     }
-    public int Get_appleY()
+    public char GetSnakeDir()
     {
-        return appleY;
+        return dir;
+    }
+  
+    public void checkApple()
+    {
+        if(apple.equals(Player.getHead())) 
+        {
+            Player.growSnake();
+            GenApple();
+        }
+    }
+    public double Get_appleX()
+    {
+        return apple.getX();
+    }
+    public double Get_appleY()
+    {
+        return apple.getY();
     }
     public void GenApple()
     {
         Random ran=new Random();
         appleX=ran.nextInt((int)(Calc_RightQuarter())-Calc_QuarterScreen())+Calc_QuarterScreen();
-        appleX=appleX*UNIT_SIZE;
+        
         appleY=ran.nextInt((int)(Screen_Height/UNIT_SIZE))*UNIT_SIZE;
+        apple.setLocation(appleX*UNIT_SIZE,appleY);
     }
 }
